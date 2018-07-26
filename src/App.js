b6e52be9c7167e5e53c7da416123137220e4bc9b
Joe Dodd <joehdodd@gmail.com>
2018-07-25 21:02:09 -0400

    hero image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import Hero from "./Hero";
 import "./App.css";
+import gc2 from "./assets/gc-2.jpg";
 
 class App extends Component {
   render() {
-    return <div>Hi!</div>;
+    return (
+      <div>
+        <Hero mainText="More Than Just Moments" img={gc2} />
+      </div>
+    );
   }
 }
 
---
+++ src/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Hero.css';
+
+const Hero = props => (
+  <div className="hero-wrapper">
+    <div className="hero-container">
+      <img className="hero-image" src={props.img}/>
+      <span className="hero-text">{props.mainText}</span>
+    </div>
+  </div>
+);
+
+export default Hero;
